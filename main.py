d36c7ef016133ba363f3c1faf68256d8d10aa6de
Ronak Wani <117783872+ronak-wani@users.noreply.github.com>
2025-11-16 09:55:01 -0500

    Added RAG blueprint

---
+++ main.py
@@ -0,0 +1,27 @@
+class RAG:
+    def __init__(self, type):
+        type = self.type
+
+    def data_loader(self):
+        pass
+
+    def vectordb(self):
+        pass
+
+    def semantic_search(self):
+        match type:
+            case "cosine":
+                pass
+            case "euclidean":
+                pass
+            case "dot_product":
+                pass
+            case _:
+                print("Type of embedding similarity needs to be specified")
+
+        pass
+
+if __name__=="__main__":
+    RAG rag_cosine = RAG("cosine")
+    RAG rag_euclidean = RAG("euclidean")
+    RAG rag_dot_product = RAG("dot_product")
