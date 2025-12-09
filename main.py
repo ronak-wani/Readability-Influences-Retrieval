87d513b48ff47907ebe6a9241cff579ca69e8299
MattKiszla <54632546+MattKiszla@users.noreply.github.com>
2025-12-09 18:42:08 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+
 import json
+import numpy as np
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnablePassthrough
@@ -6,20 +8,32 @@ from tqdm import tqdm
 from langchain_chroma import Chroma
 from langchain_community.document_loaders import JSONLoader
 from langchain_ollama import OllamaEmbeddings
+from langchain_community.retrievers import BM25Retriever, TFIDFRetriever
+
+TOP_K = 3
 
 class RAG:
     type = None
     llm = None
     docs = []
     ids = []
+    vectorstore = None
+    bm25 = None
+    tfidf = None
+    search = None
+    results = None
+
     def __init__(self, type, llm):
         self.type = type
         self.llm = llm
         self.docs, self.ids = self.data_loader()
         self.retriever = self.vectordb()
+        self.bm25 = BM25Retriever.from_documents(self.docs)
+        self.bm25.k = TOP_K
+        self.tfidf = TFIDFRetriever.from_documents(self.docs)
+        self.tfidf.k = TOP_K
         self.semantic_search()
-        self.rag_chain("./questions.json")
-        # self.results = self.evaluation()
+        self.results = self.evaluation()
 
     def data_loader(self):
         ids=[]
@@ -50,11 +64,11 @@ class RAG:
 
     def vectordb(self):
         vectorstore = Chroma(
-            collection_name="readability-rag",
-            embedding_function=OllamaEmbeddings(model='snowflake-arctic-embed', base_url="http://localhost:11434"),
-            persist_directory="./chroma_db",
-
+            collection_name="readability-rag-" + self.llm,
+            embedding_function=OllamaEmbeddings(model=self.llm, base_url="http://localhost:11434"),
+            persist_directory="./chroma_db_" + self.llm.replace("-", "_"),
         )
+        self.vectorstore = vectorstore
         existing_doc_count = vectorstore._collection.count()
 
         if existing_doc_count > 0:
@@ -74,13 +88,17 @@ class RAG:
         return retriever
 
     def semantic_search(self):
-        match type:
+        match self.type:
             case "cosine":
-                pass
+                self.search = self.retriever
             case "euclidean":
-                pass
+                self.search = self.retriever
             case "dot_product":
-                pass
+                self.search = self.retriever
+            case "bm25":
+                self.search = self.bm25
+            case "tfidf":
+                self.search = self.tfidf
             case _:
                 print("Type of embedding similarity needs to be specified")
 
@@ -101,7 +119,7 @@ class RAG:
         for topic, questions in data.items():
             for q_type, question in questions.items():
                 print(f"Question: {question}")
-                response = self.retriever.invoke(f"{question}")
+                response = self.search.invoke(f"{question}")
                 print(f"Response: {response}")
 
             print()
@@ -117,10 +135,119 @@ class RAG:
             # responses.append(response)
             return responses
 
+    
     def evaluation(self):
-        pass
+            with open("./questions.json", "r") as f:
+                questions = json.load(f)
+
+            embeddings = None
+            if self.type in ["cosine", "euclidean", "dot_product"]:
+                collection = self.vectorstore._collection
+                all_ids = collection.get()["ids"]
+                data = collection.get(include=["embeddings"], ids=all_ids)
+                embeddings = np.array(data["embeddings"])
+
+            results = {}
+
+            for title, questions in tqdm(questions.items(), desc=f"Testing {self.type}"):
+                
+                title_result = {}
+                
+
+                for query_level, question in questions.items():
+                    if self.type == "bm25":
+                        docs = self.bm25.invoke(question)[:3]
+                        retrieved_docs = [(d, 0.0) for d in docs]
+
+                    elif self.type == "tfidf":
+                        docs = self.bm25.invoke(question)[:3]
+                        retrieved_docs = [(d, 0.0) for d in docs] # tuple made even though i don't need a score.
+
+                    else:
+                        query_vector = np.array(self.vectorstore._embedding_function.embed_query(question))
+                        if self.type == "cosine":
+                            a = query_vector
+                            b = embeddings
+                            a_norm = a / (np.linalg.norm(a))
+                            b_norm = b / (np.linalg.norm(b, axis=1, keepdims=True))
+                            simularities = np.dot(b_norm, a_norm)
+                            order = np.argsort(-simularities)[:3]
+                            retrieved_docs = [(self.docs[int(i)], float(simularities[i])) for i in order]
+                        elif self.type == "dot_product":
+                            simularities = embeddings @ query_vector
+                            order = np.argsort(-simularities)[:3]
+                            retrieved_docs = [(self.docs[int(i)], float(simularities[i])) for i in order]
+                        elif self.type == "euclidean":
+                            distances = np.linalg.norm(embeddings - query_vector, axis=1)
+                            order = np.argsort(distances)[:3]
+                            retrieved_docs = [(self.docs[int(i)], float(distances[i])) for i in order]
+
+                    top_k_docs = []
+                    for rank, (document, score) in enumerate(retrieved_docs):
+                        top_k_docs.append({
+                                "rank": rank+1,
+                                "title": document.metadata.get("title"),
+                                "level": document.metadata.get("level"),
+                                "score": score,
+                            })
+                        
+
+                    num_relevant_retrieved = 0
+                        
+                    for document in top_k_docs:
+                        if(document.get("title") == title):
+                            num_relevant_retrieved += 1
+
+
+                    
+                    precision = num_relevant_retrieved / 3
+                
+                    recall = num_relevant_retrieved / 3
+                    
+
+                    level_ranks = {"Adv": None, "Int": None, "Ele": None}
+                    for rank, document in enumerate(top_k_docs):
+                        if document.get("title") == title:
+                            lvl = document.get("level")
+                            level_ranks[lvl] = rank+1
+
+                    title_result[query_level] = {
+                        "question": question,
+                        "topic": title,
+                        "query_level": query_level,
+                        "top_k_docs": top_k_docs,
+                        "metrics": {
+                            "precision_at_k=3": precision,
+                            "recall_at_k=3": recall,
+                            "num_relevant": 3,
+                            "level_ranks": level_ranks,
+                        },
+                    }
+
+                results[title] = title_result
+
+            output = {
+                "config": {
+                    "model": self.llm,
+                    "metric": self.type,
+                    "k": 3,
+                },
+                "results": results,
+            }
+
+            filename = f"results_snowflake_arctic_embed_{self.type}.json"
+            with open(filename, "w") as f:
+                json.dump(output, f, indent=4)
+
+            return output
+
+
+
+
 
 if __name__=="__main__":
-    rag_cosine = RAG("cosine", "phi4")
-    rag_euclidean = RAG("euclidean", "phi4")
-    rag_dot_product = RAG("dot_product", "phi4")
+    models = ["snowflake-arctic-embed"]
+    types = ["cosine", "euclidean", "dot_product", "bm25", "tfidf"]
+    for m in models:
+        for t in types:
+            RAG(t, m)
